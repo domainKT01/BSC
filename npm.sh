a645a1a707022be4151d7ef7d4b1283baf3236db
domainKT01 <luisdanielcoronelposada903@gmail.com>
2022-01-14 16:52:34 -0500

    step5

---
+++ npm.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+cd /c/xampp/htdocs/Cordoba_BB/node-v16.13.2-win-x64
+
+npm install && npm run dev
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,8 @@ Route::get('/', [pagesController::class, 'SignUp']);
 Route::post('forms.store', [formController::class, 'store'])->name('forms.store');
 
 Route::get('log', [pagesController::class, 'LogIn'])->name('page.login');
+
+Route::get('nada', function(){
+
+    view('auth.login');
+});
